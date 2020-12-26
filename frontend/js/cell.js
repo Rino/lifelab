d74c6c797ab2a7b85d144458887194782059c56e
Alexander Fedorenko <pyfagorus@gmail.com>
2020-12-26 17:24:39 +0300

    3d illusion

--- frontend/js/cell.js
+++ frontend/js/cell.js
@@ -7,12 +7,12 @@ class Cell {
         this.genome = genome
         this.color = genome['color']
         this.breed_factor = genome['breed_factor']
-        console.log(genome);
+        this.breed_direction = genome['breed_direction']
     }
 
     show(ctx, cell_size) {
 
-        switch(this.color){
+        switch (this.color) {
             case 'red':
                 ctx.fillStyle = 'rgb(' + this.power + ', 0 , 0)';
                 break;
@@ -28,9 +28,29 @@ class Cell {
 
     breed(sandbox) {
         let free_spaces = sandbox.get_free_space(this.x, this.y)
-        if(free_spaces.length > 0){
-            let place = free_spaces[getRandomInt(free_spaces.length)]
-            sandbox.spawn_cell(place[0], place[1], this.genome)
+        switch (this.breed_direction) {
+            case 1:
+                if (free_spaces.length > 0) {
+                    let place = free_spaces[getRandomInt(free_spaces.length)]
+                    sandbox.spawn_cell(place[0], place[1], this.genome)
+                }
+                break;
+            case 2:
+
+                let deep_free_spaces = free_spaces.map(space => [space[0], space[1], sandbox.get_free_space(space[0], space[1])]);
+
+                const reducer = (max_len, current) => Math.max(max_len, current[2].length)
+
+                let max_free_space = deep_free_spaces.reduce(reducer, 0)
+                if (max_free_space > 0) {
+                    let candidates = deep_free_spaces.filter(space => space[2].length == max_free_space);
+
+                    // if (candidates.length > 0) {
+                        let place = candidates[getRandomInt(candidates.length)]
+                        sandbox.spawn_cell(place[0], place[1], this.genome)
+                    // }
+                }
+
         }
     }
 
@@ -46,7 +66,7 @@ class Cell {
         }
 
         if (this.power > 150) {
-            if(getRandomInt(this.breed_factor) == 1) this.breed(sandbox)
+            if (getRandomInt(this.breed_factor) == 1) this.breed(sandbox)
         }
 
 
